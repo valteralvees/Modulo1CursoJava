b8ac179533a62e2e764c75f8f70c6413d0f305c2
Valter Alves <valteralvees@outlook.com>
2022-04-19 19:36:03 -0300

    added content

---
+++ Modulo1Curso/src/modulo1curso/Modulo1Curso.java
@@ -0,0 +1,16 @@
+package modulo1curso;
+public class Modulo1Curso {
+    public static void main(String[] args) {
+        MeuExercicioJava //Classe
+        int meuExercicioJava //Variável 
+        meuexerciciojava //Nome de pacotes
+        final MEU_EXERCICIO_JAVA //constante
+                
+        //DECLARAR
+        int meutamanhoemcm = 181;
+        int meutamanhoemcm2 = (int) 181;
+        Float meutamanhoemcem = new Float(181);
+        
+    }
+    
+}

6ed7f9938c7743c09b20bd59b24984bcac6da716
Valter Alves <valteralvees@outlook.com>
2022-04-19 19:51:04 -0300

    DescontoPorPagamento

--- DescontoPorPagamento/src/descontoporpagamento/DescontoPorPagamento.java
+++ DescontoPorPagamento/src/descontoporpagamento/DescontoPorPagamento.java
@@ -1,20 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package descontoporpagamento;
+import java.util.Scanner;
+    /*Uma loja fornece desconto de 10% para pagamento a vista e de 5% para outras formas de pagamento.
 
-/**
- *
- * @author morga
- */
+Escreva um programa para receber:
+- Valor da compra
+- Forma de pagamento (1 = a vista; 2 = outras formas)
+
+Mostrar:
+- Valor original da compra
+- Valor do desconto
+- Valor a pagar*/
 public class DescontoPorPagamento {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Digite o valor da compra");
+        Scanner teclado = new Scanner(System.in);
+        int valorcompra = teclado.nextInt();
+        System.out.println("Digite a forma de pagamento, sendo 1 para A VISTA e 2 para OUTROS");
+        final int ESCOLHA = teclado.nextInt();
+        if (ESCOLHA == 1){
+            float descontoaVista = valorcompra/100 *10;
+            float valorFinal = valorcompra - descontoaVista;
+            System.out.println("VALOR ORIGINAL -----R$" + valorcompra);
+            System.out.println("VALOR DO DESCONTO -----R$" + descontoaVista);
+            System.out.println("VALOR FINAL -----R$" + valorFinal);
+        } else if (ESCOLHA ==2 ){
+            float descontoaVista = valorcompra/100 *5;
+            float valorFinal = valorcompra - descontoaVista;
+            System.out.println("VALOR ORIGINAL -----R$" + valorcompra);
+            System.out.println("VALOR DO DESCONTO -----R$" + descontoaVista);
+            System.out.println("VALOR FINAL -----R$" + valorFinal);
+        }
+        
     }
     
 }
